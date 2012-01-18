98aa3c334671b24b1cf61552148dff4a39268a0d
Filip Krikava <krikava@gmail.com>
2012-01-18 15:55:21 +0100

    Improved monitor

--- src/net.fikovnik.projects.taco.latex.core/src/net/fikovnik/projects/taco/latex/core/LatexGenerator.java
+++ src/net.fikovnik.projects.taco.latex.core/src/net/fikovnik/projects/taco/latex/core/LatexGenerator.java
@@ -15,6 +15,7 @@ import net.fikovnik.projects.taco.latex.core.main.Main;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.URI;
 
@@ -27,42 +28,47 @@ public final class LatexGenerator {
 	private final IFile ecoreFile;
 	private final File targetFolder;
 	private final LatexExportProperties properties;
-	private final IGraphwizService graphwiz;
+	private final IGraphwizService graphwizService;
 
 	private final File pdfOutputFolder;
 	private final File dotOutputFolder;
 	
-	public LatexGenerator(IFile ecoreFile, File targetFolder, LatexExportProperties properties, IGraphwizService graphwiz) {
+	public LatexGenerator(IFile ecoreFile, File targetFolder, LatexExportProperties properties, IGraphwizService graphwizService) {
 		this.ecoreFile = ecoreFile;
 		this.targetFolder = targetFolder;
 		this.properties = properties;
-		this.graphwiz = graphwiz;
+		this.graphwizService = graphwizService;
 		
 		this.pdfOutputFolder = new File(targetFolder,PDF_OUTPUT_FOLDER_NAME);
 		this.dotOutputFolder = new File(targetFolder,DOT_OUTPUT_FOLDER_NAME);
 	}
 
 	public void generate(IProgressMonitor monitor) throws Exception {
-		URI modelURI = URI.createPlatformResourceURI(ecoreFile.getFullPath()
-				.toString(), true);
+		SubMonitor mainMonitor = SubMonitor.convert(monitor, 100);
+		
+		URI modelURI = URI.createPlatformResourceURI(ecoreFile.getFullPath().toString(), true);
 		List<Object> arguments = Collections.<Object> emptyList();
 		
 		Main delegate = new Main(modelURI, targetFolder, arguments);
 		delegate.getProperties().add(serializeProperty(serialize(properties)).getAbsolutePath());
 		
-		delegate.doGenerate(BasicMonitor.toMonitor(monitor));
+		delegate.doGenerate(BasicMonitor.toMonitor(mainMonitor.newChild(50)));
 		
-		processDotFiles();
+		processDotFiles(mainMonitor.newChild(50));
+		mainMonitor.done();
 	}
 
-	private void processDotFiles() {
+	private void processDotFiles(IProgressMonitor monitor) {
 		if (!dotOutputFolder.canRead()) {
+			monitor.done();
 			return;
 		}
 		
 		if (!pdfOutputFolder.exists()) {
 			if (!pdfOutputFolder.mkdirs()) {
 				// TODO: handle
+				monitor.done();
+				return;
 			}
 		}
 		
@@ -73,17 +79,25 @@ public final class LatexGenerator {
 				return file.getName().endsWith(".dot");
 			}
 		});
+
+		monitor.beginTask("Generating class diagrams", files.length);
 		
-		IGraphwiz g = graphwiz.getGraphwiz();
-		for (File in : files) {
+		IGraphwiz g = graphwizService.getGraphwiz();
+		for (File source : files) {
 			try {
-				String inFileName = in.getName();
-				File out = new File(pdfOutputFolder, inFileName.substring(0, inFileName.lastIndexOf(".dot"))+".pdf");
-				g.generate(in, out);
+				String sourceFileName = source.getName();
+				String targetFileName = sourceFileName.substring(0, sourceFileName.lastIndexOf(".dot"))+".pdf";
+				File target = new File(pdfOutputFolder, targetFileName);
+				
+				monitor.subTask("Generating: "+targetFileName);
+				g.generate(source, target);
+				monitor.worked(1);
 			} catch (GraphwizException e) {
 				// TODO: handle
 				e.printStackTrace();
 			}
 		}
+		
+		monitor.done();
 	}
 }
