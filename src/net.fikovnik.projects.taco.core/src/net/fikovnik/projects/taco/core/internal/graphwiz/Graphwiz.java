5d877cb10190d10819d8091d1994ab1e61511b88
Filip Krikava <krikava@gmail.com>
2012-01-19 01:17:25 +0100

    Add stout and std err handling

--- src/net.fikovnik.projects.taco.core/src/net/fikovnik/projects/taco/core/internal/graphwiz/Graphwiz.java
+++ src/net.fikovnik.projects.taco.core/src/net/fikovnik/projects/taco/core/internal/graphwiz/Graphwiz.java
@@ -1,37 +1,103 @@
 package net.fikovnik.projects.taco.core.internal.graphwiz;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import net.fikovnik.projects.taco.core.graphwiz.GraphwizException;
 import net.fikovnik.projects.taco.core.graphwiz.IGraphwiz;
 
+import org.apache.commons.io.IOUtils;
+
 public final class Graphwiz implements IGraphwiz {
 
+	private static class StreamGobbler extends Thread {
+		private final InputStream is;
+		private final StringWriter sw = new StringWriter();
+		private AtomicBoolean stop = new AtomicBoolean(false);
+
+		public StreamGobbler(InputStream is) {
+			this.is = is;
+		}
+
+		public void shutdown() {
+			this.stop.set(true);
+		}
+		
+		public void run() {
+			try {
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				String line = null;
+				while ((line = br.readLine()) != null && !stop.get()) {
+					sw.write(line+"\n");
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			} finally {
+				IOUtils.closeQuietly(is);
+				IOUtils.closeQuietly(sw);
+			}
+		}
+		
+		@Override
+		public String toString() {
+			return sw.toString();
+		}
+	}
+
 	private final String dotPath;
-	
+
 	public Graphwiz(String dotPath) {
 		this.dotPath = dotPath;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.fikovnik.projects.taco.core.internal.graphwiz.IGraphwitz#generatePDF(java.io.File, java.io.File)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.fikovnik.projects.taco.core.internal.graphwiz.IGraphwitz#generatePDF
+	 * (java.io.File, java.io.File)
 	 */
 	@Override
 	public void generate(File in, File out) throws GraphwizException {
 		Runtime r = Runtime.getRuntime();
+		StreamGobbler stderr = null;
+		StreamGobbler stdout = null;
+		
 		try {
-			Process p = r.exec(String.format("%s -T pdf %s -o %s", dotPath, in.getAbsolutePath(), out.getAbsolutePath()));
+			Process p = r.exec(String.format("%s -T pdf %s -o %s", dotPath,
+					in.getAbsolutePath(), out.getAbsolutePath()));
+			
+			stderr = new StreamGobbler(p.getErrorStream());
+			stderr.start();
+			stdout = new StreamGobbler(p.getInputStream());
+			stdout.start();
+			
 			int exitCode = p.waitFor();
+			
 			if (exitCode != 0) {
-				// TODO: stderr
-				throw new GraphwizException("Unable to generate PDF from "+in+". Dot process exited with "+exitCode);
+				throw new GraphwizException("Unable to generate PDF from " + in
+						+ ". Dot process exited with " + exitCode+"\nOutput: "+stdout.toString()+"\n"+stderr.toString());
 			}
 		} catch (IOException e) {
-			throw new GraphwizException("Unable to generate PDF from "+in+".", e);
+			throw new GraphwizException("Unable to generate PDF from " + in
+					+ ".", e);
 		} catch (InterruptedException e) {
-			throw new GraphwizException("Unable to generate PDF from "+in+". Process interrupted", e);
+			throw new GraphwizException("Unable to generate PDF from " + in
+					+ ". Process interrupted", e);
+		} finally {
+			if (stderr != null) {
+				stderr.shutdown();
+			}
+			if (stdout != null) {
+				stdout.shutdown();
+			}
 		}
 	}
-	
+
 }
